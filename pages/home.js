30c76d7038f5a44b354fc1001b6e1cd3e394a82d
jojo6550 <josiah.johnson6550@gmail>
2026-04-27 13:42:51 -0500

    furhter changes

--- pages/home.js
+++ pages/home.js
@@ -254,7 +254,7 @@ export const home = {
       <div class="highlight-icon"><svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/></svg></div>
       <div class="highlight-content">
         <div class="highlight-label">Direct Support</div>
-        <div class="highlight-amount">Each family receives up to <strong>65,000 JMD</strong> worth in supplies(<strong>$415 USD</strong>)</div>
+        <div class="highlight-amount">Each family receives up to <strong>65,000 JMD</strong> worth of <strong>baby supplies</strong>(<strong>$415 USD</strong>)</div>
       </div>
     </div>
   </div>
